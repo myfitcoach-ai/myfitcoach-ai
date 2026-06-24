fbc2d483cf82d10b10d2007f9231169f7ccabc06
amsingh <amandeep.singh2>
2026-06-24 12:31:01 -0400

    backend login and register done

---
+++ backend/MyFitCoach.API/Controllers/AuthController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyFitCoach.API.Data;
+using MyFitCoach.API.DTOs;
+using MyFitCoach.API.Models;
+
+namespace MyFitCoach.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public AuthController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterRequest request)
+    {
+        if (await _context.Users.AnyAsync(u => u.Email == request.Email))
+        {
+            return BadRequest("Email already exists.");
+        }
+
+        var user = new User
+        {
+            FullName = request.FullName,
+            Email = request.Email,
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password),
+            Role = "User",
+            CreatedAt = DateTime.UtcNow
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return Ok(new
+        {
+            message = "User registered successfully.",
+            user.Id,
+            user.FullName,
+            user.Email,
+            user.Role
+        });
+    }
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginRequest request)
+    {
+        var user = await _context.Users
+            .FirstOrDefaultAsync(u => u.Email == request.Email);
+
+        if (user == null)
+        {
+            return BadRequest("Invalid email or password.");
+        }
+
+        var isPasswordValid = BCrypt.Net.BCrypt.Verify(
+            request.Password,
+            user.PasswordHash
+        );
+
+        if (!isPasswordValid)
+        {
+            return BadRequest("Invalid email or password.");
+        }
+
+        return Ok(new
+        {
+            message = "Login successful.",
+            user.Id,
+            user.FullName,
+            user.Email,
+            user.Role
+        });
+    }
+}
--- backend/MyFitCoach.API/Program.cs
+++ backend/MyFitCoach.API/Program.cs
@@ -1,20 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using MyFitCoach.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AngularPolicy",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseCors("AngularPolicy");
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
